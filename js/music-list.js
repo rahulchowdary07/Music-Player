2946ea77d175343d0dbb4dcb376f34ac6b8d65c7
rahulchowdary07 <154776167+rahulchowdary07@users.noreply.github.com>
2024-06-29 21:38:01 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,128 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "01 - Inkem Inkem Inkem Kaavaale",
+    artist: "Sid Sriram",
+    img: "01 - Inkem Inkem Inkem Kaavaale",
+    src: "01 - Inkem Inkem Inkem Kaavaale"
+  },
+  {
+    name: "01 - Pranaamam",
+    artist: "DSP",
+    img: "01 - Pranaamam",
+    src: "01 - Pranaamam"
+  },
+  {
+    name: "01 - Rao Gari Abbayi",
+    artist: "Tippu",
+    img: "01 - Rao Gari Abbayi",
+    src: "01 - Rao Gari Abbayi"
+  },
+  {
+    name: "01 - Shiva Shambho",
+    artist: "Mike Archangelo",
+    img: "01 - Shiva Shambho",
+    src: "01 - Shiva Shambho"
+  },
+  {
+    name: "02 - Chali Chaliga",
+    artist: "DSP",
+    img: "02 - Chali Chaliga",
+    src: "02 - Chali Chaliga"
+  },
+  {
+    name: "02 - Rock On Bro",
+    artist: "Mike Archangelo",
+    img: "02 - Rock On Bro",
+    src: "02 - Rock On Bro"
+  },
+  {
+    name: "02 - Samayama",
+    artist: "Sid Sriram",
+    img: "02 - Samayama",
+    src: "02 - Samayama"
+  },
+  {
+    name: "03 - Apple Beauty",
+    artist: "Balasubramanyam",
+    img: "003 - Apple Beauty",
+    src: "03 - Apple Beauty"
+  },
+  {
+    name: "03 - Devudaa",
+    artist: "DSP",
+    img: "03 - Devudaa",
+    src: "03 - Devudaa"
+  },
+  {
+    name: "03 - Ningi Jaripadda",
+    artist: "Tippu",
+    img: "03 - Ningi Jaripadda",
+    src: "03 - Ningi Jaripadda"
+  },
+  {
+    name: "03 - Yenti Yenti",
+    artist: "Anirudh",
+    img: "03 - Yenti Yenti",
+    src: "03 - Yenti Yenti"
+  },
+  {
+    name: "04 - Ammaadi",
+    artist: "Thaman",
+    img: "04 - Ammaadi",
+    src: "04 - Ammaadi"
+  },
+  {
+    name: "05 - Ittage Recchipodham",
+    artist: "Hemachandra",
+    img: "05 - Ittage Recchipodham",
+    src: "05 - Ittage Recchipodham"
+  },
+  {
+    name: "05 - Mora Vinara",
+    artist: "Dhanunjay",
+    img: "05 - Mora Vinara",
+    src: "05 - Mora Vinara"
+  },
+  {
+    name: "05 - Naatu Naatu",
+    artist: "Sid Sriram",
+    img: "05 - Naatu Naatu",
+    src: "05 - Naatu Naatu"
+  },
+  {
+    name: "05 - Nee Selavadigi",
+    artist: "Vishal",
+    img: "05 - Nee Selavadigi",
+    src: "05 - Nee Selavadigi"
+  },
+  {
+    name: "06 -  Temper Theme",
+    artist: "Balasubramanyam",
+    img: "06 -  Temper Theme",
+    src: "06 -  Temper Theme"
+  },
+  {
+    name: "07 - Aggipulla Lanti",
+    artist: "Sagar",
+    img: "07 - Aggipulla Lanti",
+    src: "07 - Aggipulla Lanti"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
